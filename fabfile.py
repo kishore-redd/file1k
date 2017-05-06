825c4a476c9a95d7c9ce251cb9d3731f66ef4e1e
kishore-redd <kishu.kr7467@gmail.com>
2017-05-06 11:38:20 +0530

    fabric-test-project

---
+++ fabfile.py
@@ -0,0 +1,35 @@
+from fabric.api import local
+
+
+def touch():
+
+
+	  local("./manage.py")
+
+
+def commit():
+
+
+	  local("git add . && git commit -m 'fabric-test-project'")
+
+
+def push():
+
+
+	  local("git push origin master")
+
+
+def pull():
+
+
+	  local ("git pull")
+
+
+def deploy():
+
+
+  	  touch()
+  	  commit()
+  	  push()
+  	  pull()
+
